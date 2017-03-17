a087aa5f9606fab550f9f5f4f0229bc1344e2447
Andrey Popp <8mayday@gmail.com>
2017-03-17 14:13:20 +0300

    Prettier esy/Makefile
    No functional changes.

--- src/esy/Makefile.js
+++ src/esy/Makefile.js
@@ -4,50 +4,45 @@
  * @flow
  */
 
-
-const outdent = require('outdent');
+import outdent from 'outdent';
 
 export type Env = {
-  [name: string]: ?string;
+  [name: string]: ?string,
 };
 
 export type MakeRule = {
-  type: 'rule';
-  target: string;
-  command?: ?string | Array<void | null | string | Env>;
-  phony?: boolean;
-  dependencies?: Array<string>;
-  env?: Env;
-  exportEnv?: Array<string>;
-  shell?: string
+  type: 'rule',
+  target: string,
+  command?: ?string | Array<void | null | string | Env>,
+  phony?: boolean,
+  dependencies?: Array<string>,
+  env?: Env,
+  exportEnv?: Array<string>,
+  shell?: string,
 };
 
 export type MakeDefine = {
-  type: 'define';
-  name: string;
-  value: string | Array<void | null | string | Env>;
+  type: 'define',
+  name: string,
+  value: string | Array<void | null | string | Env>,
 };
 
 export type MakeFile = {
-  type: 'file';
-  target?: string;
-  filename: string;
-  dependencies?: Array<string>;
-  value: string;
+  type: 'file',
+  target?: string,
+  filename: string,
+  dependencies?: Array<string>,
+  value: string,
 };
 
 export type MakeRawItem = {|
-  type: 'raw';
-  value: string;
+  type: 'raw',
+  value: string,
 |};
 
-export type MakeItem =
-  | MakeRule
-  | MakeFile
-  | MakeDefine
-  | MakeRawItem;
+export type MakeItem = MakeRule | MakeFile | MakeDefine | MakeRawItem;
 
-function renderMakefile(items: Array<MakeItem>) {
+export function renderMakefile(items: Array<MakeItem>) {
   return items
     .map(item => {
       if (item.type === 'rule') {
@@ -70,7 +65,7 @@ function renderMakeDefine({name, value}) {
 }
 
 function renderMakeFile({filename, value, target, dependencies = []}) {
-  let id = escapeName(filename);
+  const id = escapeName(filename);
   let output = outdent`
     define ${id}__CONTENTS
     ${escapeEnvVar(value)}
@@ -85,7 +80,7 @@ function renderMakeFile({filename, value, target, dependencies = []}) {
     \tprintenv "${id}__CONTENTS" > $(@)
   `;
   if (target) {
-    output = output + `\n${target}: ${filename}`;
+    output += `\n${target}: ${filename}`;
   }
   return output;
 }
@@ -95,36 +90,36 @@ function renderMakeRawItem({value}) {
 }
 
 function renderMakeRule(rule) {
-  let {
+  const {
     target,
     dependencies = [],
     command,
     phony,
     env,
     exportEnv,
-    shell
+    shell,
   } = rule;
-  let header = `${target}: ${dependencies.join(' ')}`;
+  const header = `${target}: ${dependencies.join(' ')}`;
 
   let prelude = '';
   if (exportEnv) {
     exportEnv.forEach(name => {
-      prelude = prelude + `export ${name}\n`;
+      prelude += `export ${name}\n`;
     });
   }
 
   if (phony) {
-    prelude = prelude + `.PHONY: ${target}\n`;
+    prelude += `.PHONY: ${target}\n`;
   }
 
   if (shell != null) {
-    prelude = prelude = `${target}: SHELL=${shell}\n`;
+    prelude += `${target}: SHELL=${shell}\n`;
   }
 
   if (command != null) {
-    let recipe = escapeEnvVar(renderMakeRuleCommand(command));
+    const recipe = escapeEnvVar(renderMakeRuleCommand(command));
     if (env) {
-      let envString = renderMakeRuleEnv(env);
+      const envString = renderMakeRuleEnv(env);
       return `${prelude}${header}\n${envString}\\\n${recipe}`;
     } else {
       return `${prelude}${header}\n${recipe}`;
@@ -135,8 +130,8 @@ function renderMakeRule(rule) {
 }
 
 function renderMakeRuleEnv(env) {
-  let lines = [];
-  for (let k in env) {
+  const lines = [];
+  for (const k in env) {
     if (env[k] != null) {
       lines.push(`\texport ${k}="${env[k]}";`);
     }
@@ -148,10 +143,12 @@ function renderMakeRuleCommand(command) {
   if (Array.isArray(command)) {
     return command
       .filter(item => item != null)
-      .map(item =>
-        typeof item === 'string'
-        ? renderMakeRuleCommand(item)
-        : renderMakeRuleEnv(item))
+      .map(
+        item =>
+          typeof item === 'string'
+            ? renderMakeRuleCommand(item)
+            : renderMakeRuleEnv(item),
+      )
       .join('\\\n');
   } else {
     return command.split('\n').map(line => `\t${line};`).join('\\\n');
@@ -165,7 +162,3 @@ function escapeEnvVar(command) {
 function escapeName(name) {
   return name.replace(/[^a-zA-Z0-9]/g, '_').replace(/_+/g, '_');
 }
-
-module.exports = {
-  renderMakefile
-};
